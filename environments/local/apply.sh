40d34b03ddedf780789271dd95a7a841debabfe6
Nazarii Piontko <pyontko.nazar@gmail.com>
2020-03-26 18:04:38 +0200

    Refactor k8s NodePort to port-forward

--- environments/local/apply.sh
+++ environments/local/apply.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-helm install stable/postgresql --name todo-db -f ${BASH_SOURCE%/*}/postgres.yaml
-helm install stable/pgadmin --name todo-db-ui -f ${BASH_SOURCE%/*}/pgadmin.yaml
+# PostgreSQL
+helm install stable/postgresql --name todo-db -f ${BASH_SOURCE%/*}/postgres.yaml --wait
+nohup kubectl port-forward service/todo-db-postgresql 5432:5432 >/dev/null 2>&1 &
 
+# PG Admin
+helm install stable/pgadmin --name todo-db-ui -f ${BASH_SOURCE%/*}/pgadmin.yaml --wait
+nohup kubectl port-forward service/todo-db-ui-pgadmin 5433:80 >/dev/null 2>&1 &
+
+# Selenium
+helm install stable/selenium --name todo-bdd-driver -f ${BASH_SOURCE%/*}/selenium.yaml --wait
+nohup kubectl port-forward service/todo-bdd-driver-selenium-hub 4444:4444 >/dev/null 2>&1 &
+
+# Application
 if [[ $1 == --app ]]; then
-    helm install ${BASH_SOURCE%/*}/../../charts/app --name todo -f ${BASH_SOURCE%/*}/app.yaml
+    helm install ${BASH_SOURCE%/*}/../../charts/app --name todo -f ${BASH_SOURCE%/*}/app.yaml --wait
+    nohup kubectl port-forward service/todo-app 5000:80 >/dev/null 2>&1 &
 fi
--- environments/local/delete.sh
+++ environments/local/delete.sh
@@ -3,6 +3,10 @@
 helm delete --purge todo-db
 helm delete --purge todo-db-ui
 
+helm delete --purge todo-bdd-driver
+
 if [[ $1 == --app ]]; then
     helm delete --purge todo
 fi
+
+pkill -f "kubectl port-forward service/todo-"
